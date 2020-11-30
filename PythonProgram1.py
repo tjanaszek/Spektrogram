3cc6e80374319ac270578e7f391a58d5af62dd4c
TeresaJanaszek <teresa.janaszek@gmail.com>
2020-11-30 11:45:24 +0100

    added functions: opening soundfiles, playing soundfiles

--- PythonProgram1.py
+++ PythonProgram1.py
@@ -1,20 +1,77 @@
 import tkinter as tk
+from tkinter import filedialog as fd
 import random
 
-#import soundfile as sf
-#import sounddevice as sd
+import soundfile as sf
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wave
 
-#import matplotlib
-#matplotlib.use('TKAgg')
-#from matplotlib.backends.backend_tkagg import FigureCanvasTKAgg
+class Nagranie:
+    nazwa = " "
 
 class Aplikacja(tk.Frame):
-   
+
+    nagranie = Nagranie
+
     def fun(self, arg1):
         print("test "+str(arg1))
 
     def fun2(self, arg1):
         print("test "+ arg1)
+
+    def otwórz(self):
+        name = fd.askopenfilename()
+        if name:
+            #data, fs = sf.read(name, dtype='float32')
+            #sd.play(data, fs)
+            self.nagranie.nazwa=name;
+            print(name)
+
+    def play(self):
+        if self.nagranie.nazwa!= " ":
+            data, fs = sf.read(self.nagranie.nazwa, dtype='float32')
+            sd.play(data, fs)
+
+    def recordFun(self, fs, s):
+        #fs=44100 #próbki/s
+        #s = 3
+        print("nagranie 1")
+        nagranie = sd.rec(int(fs*s), samplerate=fs, channels=2)
+        sd.wait()
+        write("filename1.wav", fs, nagranie)
+        #files.append('filename1.wav')
+        print("nagranie 2")
+        nagranie = sd.rec(int(fs*s), samplerate=fs, channels=2)
+        sd.wait()
+        write("filename2.wav", fs, nagranie)
+        #files.append('filename2.wav')
+        #files.append(nagranie)
+        filename1="filename1.wav"
+        filename2="filename2.wav"
+        files =[filename1, filename2]
+        outfile="wyjsciowy.wav"
+        data=[]
+        for file in files:
+            print("tutaj")
+            with wave.open(file, "rb") as w:
+                data.append([w.getparams(), w.readframes(w.getnframes())])
+        with wave.open(outfile, "wb") as output:
+            output.setparams(data[0][0])
+            for i in range(len(data)):
+                output.writeframes(data[i][1])
+        #nagranie = sd.rec(int(fs*s), samplerate=fs, channels=2)
+        #sd.wait()
+        #write("test.wav", fs, nagranie)
+        sd.play(nagranie)
+        #sd.wait()
+
+    def nagraj(self, fs, s):
+        from scipy.io.wavfile import write
+        self.btn3['state'] = 'normal'
+        nagranie = sd.rec(int(fs * s), samplerate=fs, channels=2)
+        sd.wait()
+        sd.play(nagranie, fs)
 
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
@@ -24,10 +81,8 @@ class Aplikacja(tk.Frame):
         menubar = tk.Menu(self)
 
         submenu1 = tk.Menu(menubar, tearoff=0)
-        submenu1.add_command(label="Otwórz", command=lambda:self.fun2("test 11"))
+        submenu1.add_command(label="Otwórz", command=lambda:self.otwórz())
         submenu1.add_command(label="Zapisz", command=lambda:self.fun2("test 12"))
-        #submenu1.add_separator()
-        #submenu1.add_command(label="Test 3", command=lambda:self.fun2("test 13"))
         menubar.add_cascade(label="Plik", menu=submenu1)
 
         submenu2 = tk.Menu(menubar, tearoff=0)
@@ -44,33 +99,21 @@ class Aplikacja(tk.Frame):
 
         self.parent.config(menu=menubar)
     
-        btn1 = tk.Button(self, text="Play", command=lambda:self.fun(1), state=tk.ACTIVE)
+        btn1 = tk.Button(self, text="Play", command=lambda:self.play(), state=tk.ACTIVE)
         btn1.place(x=2, y=2, width=50, height=25)
         btn2 = tk.Button(self, text="Pause", command=lambda:self.fun(1), state=tk.ACTIVE)
         btn2.place(x=52, y=2, width=50, height=25)
-        btn3 = tk.Button(self, text="Nagraj", command=lambda:self.fun(1), state=tk.ACTIVE)
-        btn3.place(x=102, y=2, width=50, height=25)
+        self.btn3 = tk.Button(self, text="Nagraj", command=lambda:self.nagraj(44100, 2), state=tk.ACTIVE)
+        self.btn3.place(x=102, y=2, width=50, height=25)
         btn4 = tk.Button(self, text=" ", command=lambda:self.fun(1), state=tk.ACTIVE)
         btn4.place(x=152, y=2, width=50, height=25)
         btn5 = tk.Button(self, text=" ", command=lambda:self.fun(1), state=tk.ACTIVE)
         btn5.place(x=202, y=2, width=50, height=25)
 
-        #lbl = tk.Label(self, text="label 1", anchor=tk.E)
-        #lbl.place(x=100, y=50, width=100, height=25)
-        # tk.Button
-        # tk.Label
-        # tk.Menu
-        # tk.Radiobutton
-        # tk.Checkbutton
-        # tk.Entry
 
-        widget1 = tk.Canvas(self, bg="blue", height=100).place(x= 10, y=30, width=500, height=300)
+        widget1 = tk.Canvas(self, bg="white", height=100).place(x= 10, y=30, width=500, height=300)
         #lbl = tk.Label(self, text="Opis pliku dźwiękowego", anchor=tk.E).grid(column=1, columnspan=6, sticky="w")
-        widget2 = tk.Canvas(self, bg="black").place(x= 10, y=350, width=500, height=80)
-
-        # grid // kratka
-        # place// dokładnie ustawiamy
-        # pack //
+        widget2 = tk.Canvas(self, bg="white").place(x= 10, y=350, width=500, height=80)
 
 
 
@@ -80,9 +123,46 @@ class Aplikacja(tk.Frame):
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("Spektrogram")
-    root.geometry("600x450")
-    root.resizable(False,False)
-    app = Aplikacja(root)
-    root.mainloop()
+
+   root = tk.Tk()
+   root.title("Spektrogram")
+   root.geometry("600x450")
+   root.resizable(False,False)
+   app = Aplikacja(root)
+   root.mainloop()
+
+   
+   # otwarcie pliku
+   #filename1 = "miasto_słońca.wav"
+   #data, fs = sf.read(filename1, dtype='float32')
+   #print(fs)
+   #sd.play(data, fs)
+   #sd.wait()
+
+   #nagranie pliku
+   #fs=44100 #próbki/s
+   #s = 3
+   #nagranie = sd.rec(int(fs*s), samplerate=fs, channels=2)
+   #sd.wait()
+   #write("test.wav", fs, nagranie)
+
+   """
+   #łączenie plików
+   filename1 = "test.wav"
+   filename2 = "filename2.wav"
+   files = [filename1, filename2]
+   outputfile = "test2.wav"
+   data = []
+
+   for file in files:
+       #w = wave.open(file, "rb")
+       #w.close()
+       with wave.open(file, 'r') as w:
+           data.append([w.getparams(), w.readframes(w.getnframes())])
+
+   with wave.open(outputfile, 'wb') as output:
+        output.setparams(data[0][0])
+        for i in range(len(data)):
+            output.write(frames(data[i][1]))
+  
+            """
